52dfb1281e076774d7a5e95882d50a0c7f4b549a
samergain <samersadon@gmail.com>
2021-06-11 15:33:13 -0400

    adding movies class file

---
+++ movies.js
@@ -0,0 +1,14 @@
+function Movie(title,director,genre,releaseYear,rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+}
+
+Movie.prototype.getOverview = function(){
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`;
+}
+
+const Spiderman = new Movie("Spiderman","Sam Raimi", "Action", 2002, 87);
+console.log(Spiderman.getOverview());
